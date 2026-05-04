629c849be38a4dcfa0be0a8815520f5f33b52568
Muhammad Ammad <154667373+Muhammad-Ammad3@users.noreply.github.com>
2026-05-04 19:07:37 +0500

    updates

--- inngest/functions.js
+++ inngest/functions.js
@@ -3,8 +3,10 @@ import { prisma } from "@/lib/prisma";
 import { inngest } from "./client";
 
 export const syncUserCreation = inngest.createFunction(
-  { id: "sync-user-create" },
-  { event: "clerk/user.created" },
+  { 
+    id: "sync-user-create",
+    triggers: { event: "clerk/user.created" },
+  },
   async ({ event }) => {
     const { data } = event;
     await prisma.user.create({
@@ -19,8 +21,10 @@ export const syncUserCreation = inngest.createFunction(
 );
 
 export const syncUserUpdation = inngest.createFunction(
-  { id: "sync-user-update" },
-  { event: "clerk/user.updated" },
+  { 
+    id: "sync-user-update",
+    triggers: { event: "clerk/user.updated" },
+  },
   async ({ event }) => {
     const { data } = event;
     await prisma.user.update({
@@ -35,16 +39,18 @@ export const syncUserUpdation = inngest.createFunction(
     });
   },
 );
+
 export const syncUserDeletion = inngest.createFunction(
-  { id: "sync-user-delete" },
-  { event: "clerk/user.deleted" },
+  { 
+    id: "sync-user-delete",
+    triggers: { event: "clerk/user.deleted" },
+  },
   async ({ event }) => {
     const { data } = event;
     await prisma.user.delete({
       where: {
         id: data.id,
       },
-     
     });
   },
 );
