f08fc6c987d21f5353e17763b03962979ec3cfc4
Muhammad Ammad <154667373+Muhammad-Ammad3@users.noreply.github.com>
2026-05-07 22:47:24 +0500

    updates

---
+++ app/api/admin/stores/route.js
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+import authAdmin from "@/middelwares/authAdmin";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+// get all approved stores
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+    const isAdmin = await authAdmin(userId);
+    if (!isAdmin) {
+      return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+    }
+
+    const store = await prisma.store.findMany({
+      where: { status: "approved"},
+      include: { user: true },
+    });
+
+    return NextResponse.json({ store });
+  } catch (error) {
+        console.error(error)
+        return NextResponse.json({error: error.code || error.message}, {status: 400})
+    }
+}
