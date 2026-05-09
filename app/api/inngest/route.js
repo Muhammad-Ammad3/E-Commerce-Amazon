0cf2b718a5f301ef8db0a50bd4f1c1307764760e
Muhammad Ammad <154667373+Muhammad-Ammad3@users.noreply.github.com>
2026-05-09 18:24:27 +0500

    changes in Inngest.

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { deleteCouponOnExpiry, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+import {
+  deleteCouponOnExpiry,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/inngest/functions";
+import { inngest } from "@/inngest/client";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
