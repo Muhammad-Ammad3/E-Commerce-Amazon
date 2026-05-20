4f6a3de7f66783281bc0b6b8189d8981009a68f0
Muhammad Ammad <154667373+Muhammad-Ammad3@users.noreply.github.com>
2026-05-20 23:37:08 +0500

    updated

--- app/api/store/ai/route.js
+++ app/api/store/ai/route.js
@@ -79,7 +79,7 @@
 
 
 
-import authSeller from "@/middlewares/authSeller";
+import authSeller from "@/middelwares/authSeller";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import { openai } from "@/configs/openai";
